b3c65beff937c499e8c301e81828294a5e18352f
songseop.lee <lss3070@hanmail.net>
2021-05-24 22:42:07 +0900

    prod build

--- src/pages/owner/my-restaurants.tsx
+++ src/pages/owner/my-restaurants.tsx
@@ -1,12 +1,10 @@
-import { gql, useApolloClient, useQuery } from "@apollo/client";
-import React, { useEffect } from "react";
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
 import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
 import { Restaurant } from "../../components/restaurant";
 import { RESTAURANT_FRAGMENT } from "../../fragments";
 import { myRestaurants } from '../../__generated__/myRestaurants';
-import { Restaurants } from '../client/restaurants';
-import { MyRestaurant } from './my-restaurant';
 
 export const MY_RESTAURANTS_QUERY =gql`
     query myRestaurants{
